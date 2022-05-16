993c2feb4c186b6054c6e0b0a8db133ae2ceddab
ol3gatron <olegatron@gmail.com>
2022-05-16 22:17:54 +0300

    created basic design

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const todoFactory = function(title, desc, due, priority) {
  return {title, desc, due, priority}
 }
 
+const todos = []
+
 form.addEventListener("submit", function(e) {
   const title = document.querySelector("#title").value
   const desc = document.querySelector("#desc").value
   const due = document.querySelector("#due").value
   const priority = document.querySelector("input[name='priority']:checked").value
 
-
-
-  console.log(todoFactory(title, desc, due, priority))
+  todos.push(todoFactory(title, desc, due, priority))
 
   e.preventDefault()
 })
